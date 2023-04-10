6dceccff7ebe17ae42e941a286b11e937d77bb8e
VasilenkoYurii <yura.vasilenko98@gmail.com>
2023-04-10 14:10:31 +0300

    add hooks

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,28 @@
-import { Component } from 'react';
+import { useState, useEffect } from 'react';
 import { nanoid } from 'nanoid';
 import Notiflix from 'notiflix';
 import Section from 'components/Section/Section';
 import ContactsList from 'components/ContactsList/ContactsList';
 import Filter from 'components/Filter/Filter';
 import ContactForm from 'components/ContactForm/ContactForm';
+import { getVisibleContacts } from 'helperFunctions/helperFunctions';
 
-export class App extends Component {
-  state = {
-    contacts: [],
-    filter: '',
-  };
+export function App() {
+  const [contacts, setContacts] = useState(
+    JSON.parse(localStorage.getItem('contacts')) ?? []
+  );
+  const [filter, setFilter] = useState('');
+  const [visibleContacts, setVisibleContacts] = useState(
+    JSON.parse(localStorage.getItem('contacts')) ?? []
+  );
 
-  componentDidMount() {
-    const contacts = localStorage.getItem('contacts');
-    const parsedContacts = JSON.parse(contacts);
+  useEffect(() => {
+    localStorage.setItem('contacts', JSON.stringify(contacts));
+    setVisibleContacts(getVisibleContacts(contacts, filter));
+  }, [contacts, filter]);
 
-    if (parsedContacts) {
-      this.setState({ contacts: parsedContacts });
-    }
-  }
-
-  componentDidUpdate(prevProps, prevState) {
-    if (this.state.contacts !== prevState.contacts) {
-      localStorage.setItem('contacts', JSON.stringify(this.state.contacts));
-    }
-  }
-
-  addContact = values => {
-    for (const contact of this.state.contacts) {
+  const addContact = values => {
+    for (const contact of contacts) {
       if (values.name.toLowerCase() === contact.name.toLowerCase()) {
         return Notiflix.Notify.failure(`${values.name} is already in contact`);
       } else if (values.number.toLowerCase() === contact.number.toLowerCase()) {
@@ -38,52 +32,34 @@ export class App extends Component {
       }
     }
 
-    this.setState(prevState => ({
-      contacts: [...prevState.contacts, { ...values, id: nanoid() }],
-    }));
+    setContacts([...contacts, { ...values, id: nanoid() }]);
     Notiflix.Notify.success(
       'Super, the contact has been added to the contact list'
     );
   };
 
-  deleteContact = (contactId, contactName) => {
-    this.setState(prevState => ({
-      contacts: prevState.contacts.filter(contact => contact.id !== contactId),
-    }));
+  const deleteContact = (contactId, contactName) => {
+    setContacts(contacts.filter(contact => contact.id !== contactId));
     Notiflix.Notify.success(`Contact ${contactName} deleted successfully`);
   };
 
-  onChangeFind = e => {
-    this.setState({ filter: e.target.value });
+  const onChangeFind = e => {
+    setFilter(e.target.value);
   };
 
-  getVisibleContacts = () => {
-    const { filter, contacts } = this.state;
-    const normalizedFilter = filter.toLowerCase();
+  return (
+    <>
+      <Section title="Phonebook">
+        <ContactForm onSubmit={addContact} />
+      </Section>
 
-    return contacts.filter(contact =>
-      contact.name.toLowerCase().includes(normalizedFilter)
-    );
-  };
-
-  render() {
-    const { filter } = this.state;
-    const visibleContacts = this.getVisibleContacts();
-
-    return (
-      <>
-        <Section title="Phonebook">
-          <ContactForm onSubmit={this.addContact} />
-        </Section>
-
-        <Section title="Contacts">
-          <Filter onChangeFind={this.onChangeFind} value={filter} />
-          <ContactsList
-            contacts={visibleContacts}
-            onDeleteContact={this.deleteContact}
-          />
-        </Section>
-      </>
-    );
-  }
+      <Section title="Contacts">
+        <Filter onChangeFind={onChangeFind} value={filter} />
+        <ContactsList
+          contacts={visibleContacts}
+          onDeleteContact={deleteContact}
+        />
+      </Section>
+    </>
+  );
 }
